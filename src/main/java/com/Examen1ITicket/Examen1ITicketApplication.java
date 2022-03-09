425bc475b4a18ce743722ae8711ad51e16b72f23
fabia <fabia@LAPTOP-6G1B96NL>
2022-03-08 18:34:32 -0600

    Se crea el proyecto Examen1ITicket para el examen 1

---
+++ src/main/java/com/Examen1ITicket/Examen1ITicketApplication.java
@@ -0,0 +1,13 @@
+package com.Examen1ITicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen1ITicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen1ITicketApplication.class, args);
+	}
+
+}
